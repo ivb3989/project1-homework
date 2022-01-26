cf4c2dd9fa2ab67be5cb63b66d465f44198e5731
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-01-26 21:08:40 +0200

    finished writing test

--- myfile.test.js
+++ myfile.test.js
@@ -3,13 +3,27 @@ import { stringMerge } from "./stringMerge";
 import { createPhoneNumber } from "./createPhoneNumber";
 import { getOperands } from "./getOperands";
 import { getTime } from "./getTime";
+import { isPalindrome } from "./isPalindrome";
+import { swapCase } from "./swapCase";
+import { countOccurrences } from "./countOccurrences";
+import { vowelCount } from "./vowelCount";
+import { alphabetPosition } from "./alphabetPosition";
 
 describe("basicOp function", () => {
-  it("should return the result of numbers after applying the chosen operation", () => {
+  it("should return 11", () => {
     expect(basicOp("+", 4, 7)).toBe(11);
+  });
+  it("should return -3", () => {
     expect(basicOp("-", 15, 18)).toBe(-3);
+  });
+  it("should return 25", () => {
     expect(basicOp("*", 5, 5)).toBe(25);
+  });
+  it("should return 7", () => {
     expect(basicOp("/", 49, 7)).toBe(7);
+    expect(basicOp("something else", 45, 45)).toBe(0);
+  });
+  it("should return 0", () => {
     expect(basicOp("something else", 45, 45)).toBe(0);
   });
 });
@@ -43,13 +57,19 @@ describe("createPhoneNumber function", () => {
 });
 
 describe("getOperands function", () => {
-  it('should return its operands in the next format: "a: <first operand>, b: <second operand>"', () => {
+  it('should return "a: 1, b: 2"', () => {
     expect(getOperands("1 + 2")).toBe("a: 1, b: 2");
+  });
+  it('should return "a: 1.2, b: -3.4"', () => {
     expect(getOperands("1.2 * -3.4")).toBe("a: 1.2, b: -3.4");
+  });
+  it('should return "a: 1, b: 2"', () => {
     expect(getOperands("1 - 2")).toBe("a: 1, b: 2");
+  });
+  it('should return "a: 1, b: -2"', () => {
     expect(getOperands("1 - -2")).toBe("a: 1, b: -2");
   });
-  it("shouldn't return its operands, if anything goes wrong", () => {
+  it("should return a custom error message", () => {
     expect(getOperands("1.2 * -3.4 + 5")).toBe(
       "More than two operands'n'operators are not allowed;("
     );
@@ -58,12 +78,78 @@ describe("getOperands function", () => {
 });
 
 describe("getTime function", () => {
-  it("should return first valid time in this string", () => {
+  it('should return "09:00"', () => {
     expect(getTime("Breakfast at 09:00")).toBe("09:00");
+  });
+  it('should return "21:00"', () => {
     expect(getTime("Breakfast at 09:60, Dinner at 21:00")).toBe("21:00");
+  });
+  it('should return "09:59"', () => {
     expect(getTime("Breakfast at 09:59, Dinner at 21:00")).toBe("09:59");
   });
   it("should return empty string, if correct time not found", () => {
     expect(getTime("Breakfast at 24:00")).toBe("");
+  });
+});
+
+describe("isPalindrome function", () => {
+  it("should return true", () => {
+    expect(isPalindrome("Eva, can I see bees in a cave?")).toBe(true);
+  });
+  it("should return false", () => {
+    expect(isPalindrome("race a car")).toBe(false);
+  });
+  it("should return true", () => {
+    expect(isPalindrome("Was it a cat I saw?")).toBe(true);
+  });
+});
+
+describe("swapCase function", () => {
+  it('should return "AbC"', () => {
+    expect(swapCase("aBc")).toBe("AbC");
+  });
+  it('should return "gOOd"', () => {
+    expect(swapCase("GooD")).toBe("gOOd");
+  });
+  it('should return "HELLO"', () => {
+    expect(swapCase("hello")).toBe("HELLO");
+  });
+  it('should return "75386"', () => {
+    expect(swapCase("75386")).toBe("75386");
+  });
+});
+
+describe("countOccurrences function", () => {
+  it("should return 2", () => {
+    expect(countOccurrences("ability", "i")).toBe(2);
+  });
+  it("should return 1", () => {
+    expect(countOccurrences("abc", "a")).toBe(1);
+  });
+  it("should return 1", () => {
+    expect(countOccurrences("ABC", "a")).toBe(1);
+  });
+});
+
+describe("vowelCount function", () => {
+  it("should return 5", () => {
+    expect(vowelCount("abracadabra")).toBe(5);
+  });
+  it("should return 2", () => {
+    expect(vowelCount("letter")).toBe(2);
+  });
+  it("should return 1", () => {
+    expect(vowelCount("toy")).toBe(1);
+  });
+  it("should return 0", () => {
+    expect(vowelCount("555")).toBe(0);
+  });
+});
+
+describe("alphabetPosition function", () => {
+  it("should return the number (count) of vowels in the given string, but no y", () => {
+    expect(alphabetPosition("The sunset sets at twelve o' clock.")).toBe(
+      "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+    );
   });
 });
