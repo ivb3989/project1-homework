b41e06e5a8208e9b720b29dcfcc40a231a471e10
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-01-26 21:17:44 +0200

    corrected mistakes

--- getOperands.js
+++ getOperands.js
@@ -1,18 +1,12 @@
 export const getOperands = (expression) => {
-  const arithmetic_operators = ["+", "-", "*", "/"];
+  let arithmeticOperators = ["+", "-", "*", "/"];
   let fragments = expression.split(" ");
   if (fragments.length > 3) {
     return "More than two operands'n'operators are not allowed;(";
   }
-  if (arithmetic_operators.some((v) => fragments.includes(v))) {
+  if (arithmeticOperators.some((value) => fragments.includes(value))) {
     return `a: ${fragments[0]}, b: ${fragments[2]}`;
   } else {
     return `No match using "${expression}"`;
   }
 };
-
-/* console.log(
-  expression.map((v) =>
-    arithmetic_operators.includes(v) ? v + "is operator" : v
-  )
-); */
