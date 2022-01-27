215332a002e2f38f0cf3f13c8263ce7636435af1
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-01-27 12:13:37 +0200

    Update alphabetPosition.js

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -1,5 +1,5 @@
 export const alphabetPosition = (str) => {
-  let modifiedStr = str.toLowerCase().replace(/[^a-z]/g, "");
-  let charCodeOfA = "a".charCodeAt();
+  const modifiedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+  const charCodeOfA = "a".charCodeAt();
   return [...modifiedStr].map((letter) => letter.charCodeAt() - charCodeOfA + 1).join(" ");
 };
