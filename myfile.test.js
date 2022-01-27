473bd7202b0ed42a2be89a89ebf7094de5126bc5
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-01-27 11:13:52 +0200

    finished writing test

--- myfile.test.js
+++ myfile.test.js
@@ -49,7 +49,7 @@ describe("stringMerge function", () => {
 });
 
 describe("createPhoneNumber function", () => {
-  it('should return "(xxx) xxx-xxxx"', () => {
+  it('should return "(xxx) xxx-xxxx" when pass array with numbers', () => {
     expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toBe(
       "(123) 456-7890"
     );
